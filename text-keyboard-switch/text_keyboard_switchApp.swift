0159c913bc7c12b6190e383ed3f891dececc2351
l.korsakov <artforme2011@gmail.com>
2024-07-19 17:29:53 +0400

    Initial Commit

---
+++ text-keyboard-switch/text_keyboard_switchApp.swift
@@ -0,0 +1,17 @@
+//
+//  text_keyboard_switchApp.swift
+//  text-keyboard-switch
+//
+//  Created by Леонид Корсаков on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct text_keyboard_switchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ text-keyboard-switchUITests/text_keyboard_switchUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  text_keyboard_switchUITestsLaunchTests.swift
+//  text-keyboard-switchUITests
+//
+//  Created by Леонид Корсаков on 19.07.2024.
+//
+
+import XCTest
+
+final class text_keyboard_switchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
